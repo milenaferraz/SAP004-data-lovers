b466db5e3a8ad10ecc55ad5ee1de6553ab0c425f
Milena Ferraz <milenaferraz@gmail.com>
2020-05-07 11:59:55 -0300

    atualizacao-layout

--- src/data.js
+++ src/data.js
@@ -1,33 +1,13 @@
-
-//import data from './data/pokemon.js'
-
+//import { getData } from './main.js'
 
 
-/*
-
-export const getPokemons = (data) => {
-    let pokemon = data;
-    return pokemon.map(() => ({id,name,img,types,weight,height}));   
- 
-};
-
-export const getPokemons = () => {
-    let poke = data.pokemon;
-   
-    for(let pokemon of poke){
-        return pokemon;
+export const filtro = {
+    
+    name: (input,name) => {
+       return (name === input)
+        
     }
-
-
 };
 
 
-export const filterData(data, condition) = () => {
-  
-  
-};
 
-export const computeStats(data) = () => {
-  
-  
-} */
--- src/main.js
+++ src/main.js
@@ -1,93 +1,47 @@
 
 import data from './data/pokemon.js'
+//import { filtro } from './data.js';
+
+window.addEventListener('load', () => {
+  getData();
+});
 
 
 const pokemons = data.pokemon;
 const container = document.getElementById("container");
-const input = document.getElementById("texto").value;
-const btSearch = document.getElementById("btn-search")
-//btSearch.addEventListener("click", verificar);
+const button = document.getElementById("button");
+//const input = document.getElementById("input")
 
 
-
-for(let poke of pokemons){
-  const card = `
-              <ul class="card-lista">
-              <li class="card-inf"><img src=${poke.img} /></li>
-              <li class="card-inf">Número: ${poke.id}</li>
-              <li class="card-inf">Pokemon: ${poke.name}</li>
-              <li class="card-inf">Tipos: ${poke.type}</li>` 
-              
- container.innerHTML += card;
-
+const getData = ()=> {
+  
+  for(let poke of pokemons){
+    const img = poke.img,
+          id = poke.id,
+          name = poke.name,
+          type = poke.type;
+            
+    card(img,id,name,type);
+  }
 }
 
 
-
-
-
-
-
-/*
-
-
-
-let newElement = document.createElement("div");
-let myText = document.createTextNode(pokemons.map(function({id,name,img,types,weight,height}){
-return createCard(id,name,img,types,weight,height);
-}))
-let join = newElement.appendChild(myText);
-document.getElementById("container").appendChild(join);
-
-
-function createCard(id,name,img,types,weight,height){
-  let ul = document.createElement("ul");
-  let li = document.createElement("li");
-  li.classList.add("card-lista")
-  let photo = document.createElement("img");
-  photo.src = img;  
-  li.appendChild(photo);
-  ul.appendChild(li)
-  let liOne = document.createElement("li");
-  liOne.textContent = name;
-  document.getElementById("container").appendChild(ul);
-
-  return ul.innerHTML;
-}
-
-
-
-function verificar(){
-let resultado = search.name(input);
-    document.getElementById("result").innerHTML = resultado
-}
-*/
-
-
-
-/*
-let newElement = document.createElement("div");
-let myText = document.createTextNode(pokemons.map(function({id,name,img}){
-return `${img}Nome:${name}ID:${id}`}))
-let join = newElement.appendChild(myText);
-document.getElementById("container").appendChild(join);
-
-
-
-   
-    })*/
+const card = (img,id,name,type) => {
     
-    // const getData = pokemon.map(item => `${item.name} ${item.id}`);
-   // result.innerHTML= getData;
+    const card = `
+              <ul class="card-lista">
+              <li class="card-inf"><img src=${img} /></li>
+              <li class="card-inf">Número: ${id}</li>
+              <li class="card-inf">Pokemon: ${name}</li>
+              <li class="card-inf">Tipos: ${type}</li>` 
+    
+      container.innerHTML += card;
+
+};
 
 
-    /*
-    for(let poke of pokemon){
-      
-    }
-    */
 
-    //const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);    
-    //const yearAlive = inventors.map(({first, last, passed, year,}) => [`${first} ${last}`, passed - year]);
+button.addEventListener('click', () => {
 
-//console.log(data.pokemon[0]);
+
+});
